953a9911fe426481ff71898daded16ead731276c
jonnylloyd32 <155377168+jonnylloyd32@users.noreply.github.com>
2026-01-30 16:04:35 +0000

    Add MorseCode parameter to Decode and ReceiveMorseCode

--- Morse_Code/Morse_Code.py
+++ Morse_Code/Morse_Code.py
@@ -91,11 +91,13 @@ def GetNextLetter(i, Transmission):
         SymbolString = SymbolString + Symbol
     return i, SymbolString
 
-def Decode(CodedLetter, Dash, Letter, Dot):
+def Decode(CodedLetter, Dash, Letter, Dot, MorseCode):
     CodedLetterLength = len(CodedLetter)
     Pointer = 0
     for i in range(CodedLetterLength):
         Symbol = CodedLetter[i]
+        if Symbol not in MorseCode:
+            ReportError("invalid symbol", Symbol, "recieved.")
         if Symbol == SPACE:
             return SPACE
         elif Symbol == '-':
@@ -104,7 +106,7 @@ def Decode(CodedLetter, Dash, Letter, Dot):
             Pointer = Dot[Pointer]
     return Letter[Pointer]
 
-def ReceiveMorseCode(Dash, Letter, Dot): 
+def ReceiveMorseCode(Dash, Letter, Dot, MorseCode): 
     PlainText = EMPTYSTRING
     MorseCodeString = EMPTYSTRING
     Transmission = GetTransmission()
@@ -114,7 +116,7 @@ def ReceiveMorseCode(Dash, Letter, Dot):
     while i < LastChar:
         i, CodedLetter = GetNextLetter(i, Transmission)
         MorseCodeString = MorseCodeString + SPACE + CodedLetter
-        PlainTextLetter = Decode(CodedLetter, Dash, Letter, Dot)
+        PlainTextLetter = Decode(CodedLetter, Dash, Letter, Dot, MorseCode)
         PlainText = PlainText + PlainTextLetter
     print(MorseCodeString)
     print(PlainText)
@@ -204,7 +206,6 @@ def SendReceiveMessages():
     Dash = [20,23,0,0,24,1,0,17,0,21,0,25,0,15,11,0,0,0,0,22,13,0,0,10,0,0,0]
     Dot = [5,18,0,0,2,9,0,26,0,19,0,3,0,7,4,0,0,0,12,8,14,6,0,16,0,0,0]
     Letter = [' ','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', FULLSTOP]
-
     MorseCode = [' ','.-','-...','-.-.','-..','.','..-.','--.','....','..','.---','-.-','.-..','--','-.','---','.--.','--.-','.-.','...','-','..-','...-','.--','-..-','-.--','--..', '.-.-.-']
 
     ProgramEnd = False
@@ -212,7 +213,7 @@ def SendReceiveMessages():
         DisplayMenu() 
         MenuOption = GetMenuOption()
         if MenuOption == 'R':
-            ReceiveMorseCode(Dash, Letter, Dot)
+            ReceiveMorseCode(Dash, Letter, Dot, MorseCode)
         elif MenuOption == 'S':
             SendMorseCode(MorseCode) 
         elif MenuOption == 'P':
@@ -227,4 +228,3 @@ def SendReceiveMessages():
 
 if __name__ == "__main__":
   SendReceiveMessages()
-
